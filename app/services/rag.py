db14568e97ba2963b9cac0661817e0b340cff2fd
Jader Caro <jjadercaro192@gmail.com>
2025-06-08 18:11:21 -0500

    Global cache performance

--- app/services/rag.py
+++ app/services/rag.py
@@ -6,28 +6,28 @@ from app.services.utils import responder_pregunta_contexto
 from app.config import API_KEY
 from pathlib import Path
 
-def answer_question(question):
-    embedding_path = Path("app/services/Conformed/faq_embeddings.npy")
-    embeddings = np.load(embedding_path) 
+# Solo se cargan una vez
+embedding_path = Path("app/services/Conformed/faq_embeddings.npy")
+df_path = Path("app/services/Conformed/df_unificado.xlsx")
 
-    dimension = embeddings.shape[1]
-    index = faiss.IndexFlatL2(dimension)
-    index.add(embeddings)
+# Cache global
+model = SentenceTransformer('all-MiniLM-L6-v2')
+embeddings = np.load(embedding_path)
+dimension = embeddings.shape[1]
+index = faiss.IndexFlatL2(dimension)
+index.add(embeddings)
 
-    pregunta_usuario = str(question)
+df = pd.read_excel(df_path)
+df['text'] = "Pregunta: " + df['pregunta'] + "\nRespuesta: " + df['respuesta']
+texts = df['text'].tolist()
 
-    model = SentenceTransformer('all-MiniLM-L6-v2')
-    embedding_pregunta = model.encode([pregunta_usuario])
-
+def answer_question(question: str):
+    embedding_pregunta = model.encode([question])
     distancias, indices = index.search(embedding_pregunta, k=5)
 
-    df_path = Path("app/services/Conformed/df_unificado.xlsx")
-    df = pd.read_excel(df_path)
-    df['text'] = "Pregunta: " + df['pregunta'] + "\nRespuesta: " + df['respuesta']
-    texts = df['text'].tolist()
     textos_recuperados = [texts[i] for i in indices[0]]
-
     contexto = "\n\n".join(textos_recuperados)
-    respuesta = responder_pregunta_contexto(API_KEY, contexto, pregunta_usuario)
-    
+
+    respuesta = responder_pregunta_contexto(contexto, question)
+
     return {"respuesta": respuesta}
