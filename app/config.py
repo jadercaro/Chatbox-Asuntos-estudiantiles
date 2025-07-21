655bb3994f91942da9e373fbbf2a3b9024f019eb
Jader Caro <jjadercaro192@gmail.com>
2025-07-20 22:20:22 -0500

    Change tokens

--- app/config.py
+++ app/config.py
@@ -1,4 +1,3 @@
 #Api Key de Groq
 #API_KEY = "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"
 #API_KEY= "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"
-API_KEY = "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"
